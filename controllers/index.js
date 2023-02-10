b797b61b536fb4e15ce937b472e7d5af97d57fe0
DomSpadafora <120040191+DomSpadafora@users.noreply.github.com>
2023-02-10 15:29:54 -0500

    add some css for main page

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
-const dashboardRoutes = require('./dashboardroutes');
+// const dashboardRoutes = require('./dashboardroutes');
 const homeRoutes = require('./homeroutes');
-const apiRoutes = require('./api')
+// const apiRoutes = require('./api')
 
 router.use('/', homeRoutes);
-router.use('/dashboard', dashboardRoutes);
-router.use('/api', apiRoutes);
+// router.use('/dashboard', dashboardRoutes);
+// router.use('/api', apiRoutes);
 
 module.exports = router;
